894c6971b5293ff4ea6ef79da3971a07cc8144bf
Manasa Muddasani <manasakanthala@gmail.com>
2020-05-22 14:07:30 +0200

    add search feature

--- src/components/MovieAPI/MovieApi.js
+++ src/components/MovieAPI/MovieApi.js
@@ -1,21 +1,25 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-export default function MovieApi() {
-  const [movies, setMovies] = useState([]);
+let movies = [];
+export default function MovieApi(props) {
+  //const [movies, setMovies] = useState(null);
+  const searchKeyword = props.searchKeyword;
 
   useEffect(() => {
-    async function fetchMovies() {
+    async function fetchMovies(searchKey) {
       const result = await axios(
-        "https://api.themoviedb.org/3/trending/all/day?api_key=b92e553f9d2e936c9270209ffbc64c82"
+        //  "https://api.themoviedb.org/3/trending/all/day?api_key=b92e553f9d2e936c9270209ffbc64c82"
+        "https://api.themoviedb.org/3/search/movie?api_key=b92e553f9d2e936c9270209ffbc64c82&language=en-US&query=" +
+          searchKey +
+          "&page=1&include_adult=false"
       );
-      setMovies(result.data.results);
+      movies = result.data.results;
     }
-    fetchMovies();
-  }, []);
+    fetchMovies(searchKeyword);
+  });
   //{movies.map((movie) => movie.name).filter((movie) => movie)}
-
   console.log(movies);
 
-  return <ul></ul>;
+  return movies.map((movie) => movie.title);
 }
--- src/components/SearchMovie/SearchMovie.js
+++ src/components/SearchMovie/SearchMovie.js
@@ -1,11 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import InputBase from "@material-ui/core/InputBase";
 import { fade, makeStyles } from "@material-ui/core/styles";
-import MenuIcon from "@material-ui/icons/Menu";
 import SearchIcon from "@material-ui/icons/Search";
 
 import MovieApi from "../MovieAPI/MovieApi";
@@ -67,19 +65,17 @@ const useStyles = makeStyles((theme) => ({
 
 export default function SearchMovie() {
   const classes = useStyles();
+  const [searchKeyword, setSearchKeyword] = useState("");
+
+  const handleOnChange = (e) => {
+    const keyword = e.target.value;
+    setSearchKeyword(keyword);
+  };
 
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="open drawer"
-          >
-            {/*  <MenuIcon /> */}
-          </IconButton>
           <Typography className={classes.title} variant="h6" noWrap>
             Leo Movies
           </Typography>
@@ -94,11 +90,13 @@ export default function SearchMovie() {
                 input: classes.inputInput,
               }}
               inputProps={{ "aria-label": "search" }}
+              trpe="text"
+              onChange={handleOnChange}
             />
           </div>
         </Toolbar>
       </AppBar>
-      <MovieApi />
+      <MovieApi searchKeyword={searchKeyword} />
     </div>
   );
 }
