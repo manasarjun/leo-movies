8f510bf7c51eff9efb342204377004687046e4c3
Manasa Muddasani <manasakanthala@gmail.com>
2020-05-25 00:02:52 +0200

    api refactor

--- src/utils/apicall.js
+++ src/utils/apicall.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
 
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = 'b92e553f9d2e936c9270209ffbc64c82';
 
-let result = [];
+export default function apiCall(input) {
+  if (input) {
+    return axios(
+      `${BASE_URL}/search/movie?api_key=${API_KEY}&language=en-US&query=${input}&page=1&include_adult=false`,
+    );
+  }
 
-export default async function apiCall({ searchKeyword }) {
-  result = await axios(
-    //`https://api.themoviedb.org/3/search/movie?api_key=b92e553f9d2e936c9270209ffbc64c82&language=en-US&query=${searchKeyword}&page=1&include_adult=false`,
-  );
-
-
-  return result.data.results;
+  return axios(`${BASE_URL}/trending/all/day?api_key=${API_KEY}`);
 }
