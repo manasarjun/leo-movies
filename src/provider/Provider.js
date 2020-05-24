0fdcc01645ad6b2f53e8ba1d54fd357df8800c7d
Manasa Muddasani <manasakanthala@gmail.com>
2020-05-25 01:15:01 +0200

    refactor provider

--- src/provider/Provider.js
+++ src/provider/Provider.js
@@ -1,46 +1,86 @@
 import React, { useState } from 'react';
 
+export const FAVOURITES = 'favourites';
+export const WATCHLIST = 'watchlist';
 
-const FAVOURITES = 'favourites';
-const WATCHLIST = 'watchlist';
 let favouriteMovies = [];
+let watchListMovies = [];
+
+const getItem = (key) => {
+  if (key === FAVOURITES) {
+    return JSON.parse(window.localStorage.getItem(FAVOURITES));
+  }
+  return JSON.parse(window.localStorage.getItem(WATCHLIST));
+};
+
+const setItem = (key) => {
+  if (key === FAVOURITES) {
+    return window.localStorage.setItem(FAVOURITES, JSON.stringify(favouriteMovies));
+  }
+  return window.localStorage.setItem(WATCHLIST, JSON.stringify(watchListMovies));
+};
+
 
 export const StoreContext = React.createContext({
   favourites: [],
-  setfavourites: () => { },
+  watchList: [],
+  setFavourites: () => { },
   updateFavourites: () => { },
+  setWatchList: () => { },
+  updateWatchList: () => { },
 });
 
 
 export default function Provider(props) {
 
-
-  const getStore = () => {
-    if (window.localStorage.getItem(FAVOURITES)) {
-      favouriteMovies = JSON.parse(window.localStorage.getItem(FAVOURITES));
+  const getStore = (storeKey) => {
+    if (storeKey === FAVOURITES) {
+      if (window.localStorage.getItem(storeKey)) {
+        favouriteMovies = getItem(storeKey);
+      } else {
+        setItem(FAVOURITES);
+      }
+      return favouriteMovies;
     }
-    return favouriteMovies;
+
+    if (storeKey === WATCHLIST) {
+      if (window.localStorage.getItem(storeKey)) {
+        watchListMovies = getItem(WATCHLIST);
+      } else {
+        setItem(WATCHLIST);
+      }
+      return watchListMovies;
+    }
   };
 
-  const [favourites, setfavourites] = useState(getStore());
+  const [favourites, setFavourites] = useState(getStore(FAVOURITES));
+  const [watchList, setWatchList] = useState(getStore(WATCHLIST));
 
-  const setStore = (favo) => {
-    if (window.localStorage.getItem(FAVOURITES)) {
-      favouriteMovies = [...favourites, favo];
-      window.localStorage.setItem(FAVOURITES, JSON.stringify(favouriteMovies));
-    } else {
-      favouriteMovies.push(favo);
-      window.localStorage.setItem(FAVOURITES, JSON.stringify(favouriteMovies));
+  const setStore = (data, key) => {
+    if (key === FAVOURITES) {
+      favouriteMovies = [...favourites, data];
+      setItem(FAVOURITES);
+      setFavourites(favouriteMovies);
     }
-    setfavourites(favouriteMovies);
-    return favouriteMovies;
+
+    if (key === WATCHLIST) {
+      watchListMovies = [...watchList, data];
+      setItem(WATCHLIST);
+      setWatchList(watchListMovies);
+    }
   };
 
-  const updateStore = (movieId) => {
-    if (window.localStorage.getItem(FAVOURITES)) {
+  const updateStore = (movieId, key) => {
+    if (key === FAVOURITES) {
       const updatedMovies = favourites.filter((s) => movieId !== s.id);
       window.localStorage.setItem(FAVOURITES, JSON.stringify(updatedMovies));
-      setfavourites(updatedMovies);
+      setFavourites(updatedMovies);
+    }
+
+    if (key === WATCHLIST) {
+      const updatedWatchList = watchList.filter((s) => movieId !== s.id);
+      window.localStorage.setItem(WATCHLIST, JSON.stringify(updatedWatchList));
+      setWatchList(updatedWatchList);
     }
   };
 
@@ -48,17 +88,22 @@ export default function Provider(props) {
     <StoreContext.Provider value={
       {
         favourites,
-        setfavourites: (returnVal) => {
-          setStore(returnVal);
+        watchList,
+        setFavourites: (data, key) => {
+          setStore(data, key);
         },
-        updateFavourites: (returnVal) => {
-          updateStore(returnVal);
+        updateFavourites: (data, key) => {
+          updateStore(data, key);
+        },
+        setWatchList: (data, key) => {
+          setStore(data, key);
+        },
+        updateWatchList: (data, key) => {
+          updateStore(data, key);
         },
       }
     }>
       {props.children}
     </StoreContext.Provider>
-
-
-  )
+  );
 }
